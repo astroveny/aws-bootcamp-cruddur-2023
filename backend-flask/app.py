b682ea53115d7ba0aaac3c83d3e0b0dbda4ff00b
astroveny <b.hakam@gmail.com>
2023-03-02 12:33:52 +0000

    add watchtower and logging

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -15,6 +15,21 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+# Cloudwatch
+
+import logging
+import watchtower
+from time import strftime
+
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+#LOGGER.info("some message")
+
 # XRAY
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
@@ -67,6 +82,13 @@ cors = CORS(
   methods="OPTIONS,GET,HEAD,POST"
 )
 
+# Cloudwatch - logger: generate error
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
+
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
   user_handle  = 'andrewbrown'
@@ -109,7 +131,7 @@ def data_notifications():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
+  data = HomeActivities.run(logger=LOGGER)
   return data, 200
 
 # Honeycomb
