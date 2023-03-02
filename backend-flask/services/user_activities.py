f174bc717eb7eb145e27ff9deeb7d51fde52be62
astroveny <b.hakam@gmail.com>
2023-03-02 10:45:05 +0000

    modified segment delay time

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -2,21 +2,21 @@ from datetime import datetime, timedelta, timezone
 from aws_xray_sdk.core import xray_recorder
 from time import sleep
 
-sleep(0.05)
+
 class UserActivities:
   def run(user_handle):
     model = {
       'errors': None,
       'data': None
     }
-    sleep(0.03)
+    sleep(0.003)
   # XRAY start segment
     userseg = xray_recorder.current_segment()
     #XRAY call segment user ID
     userseg.set_user("U12345")
   # XRAY start subsegment 
     subuserseg = xray_recorder.begin_subsegment('start_time') 
-    sleep(0.01)
+    sleep(0.001)
     now = datetime.now(timezone.utc).astimezone()
 
     if user_handle == None or len(user_handle) < 1:
