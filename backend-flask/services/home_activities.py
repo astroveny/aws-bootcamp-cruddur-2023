c7d3bcdc694aa731b5f7f1019f3148f03b0bfbf5
astroveny <b.hakam@gmail.com>
2023-03-17 20:14:44 +0000

    added rds connection, SG allow gitpod IP, db-setup to run all

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
-from lib.db import pool 
+from lib.db import pool, query_wrap_array
 
 class HomeActivities:
   def run(cognito_user_id=None):
@@ -7,9 +7,23 @@ class HomeActivities:
     #logger.info("To-CW: Home Activities" )
     now = datetime.now(timezone.utc).astimezone()
     
-    sql= """
-    Select * FROM activities
-    """
+    sql = query_wrap_array("""
+        SELECT
+          activities.uuid,
+          users.display_name,
+          users.handle,
+          activities.message,
+          activities.replies_count,
+          activities.reposts_count,
+          activities.likes_count,
+          activities.reply_to_activity_uuid,
+          activities.expires_at,
+          activities.created_at
+        FROM public.activities
+        LEFT JOIN public.users ON users.uuid = activities.user_uuid
+        ORDER BY activities.created_at DESC
+        """)
+      
     with pool.connection() as conn:
        with conn.cursor() as cur:
          cur.execute(sql)
--- journal/week4.md
+++ journal/week4.md
@@ -24,7 +24,7 @@
 >> **NOTE:** output has been reduced!
 ```json
 gitpod /workspace/aws-bootcamp-cruddur-2023 (main) $ aws rds create-db-instance --db-instance-identifier cruddur-db-instance --db-instance-class db.t3.micro \
->   --engine postgres --engine-version 14.6 --master-username root --master-user-password YourPasswordHere --allocated-storage 20 --availability-zone us-east-1a \
+>   --engine postgres --engine-version 14.6 --master-username cruddurroot --master-user-password YourPasswordHere --allocated-storage 20 --availability-zone us-east-1a \
 >   --backup-retention-period 0 --port 5432 --no-multi-az --db-name cruddur --storage-type gp2 --publicly-accessible --storage-encrypted \
 >   --enable-performance-insights --performance-insights-retention-period 7 --no-deletion-protection
 
