0a15a1c1b0e953c9c67c9165379ca209b8a68868
astroveny <b.hakam@gmail.com>
2023-03-26 13:55:28 +0000

    lambda inline policy

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,7 @@
 INSERT INTO public.users (email, display_name, handle, cognito_user_id)
 VALUES
   ('woody@gmail.com', 'Woody', 'woody','7659c102-7ad4-44bd-bdc3-208f934d1f41'),
-  ('astroveny@gmail.com', 'Bachar', 'astroveny' ,'MOCK'),
+  ('astroveny@gmail.com', 'Astro Veny', 'astroveny' ,'MOCK'),
   ('buzz@gmail.com', 'Buzz Lightyear', 'buzz' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
