40a30b83aa85dfe6e9fe42cc4b2575c6b19dbe2b
astroveny <b.hakam@gmail.com>
2023-07-31 13:14:04 +0000

    cleanup week-x -p5

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,8 +2,8 @@
 INSERT INTO public.users (email, display_name, handle, cognito_user_id)
 VALUES
   ('woody@gmail.com', 'Woody', 'woody','7659c102-7ad4-44bd-bdc3-208f934d1f41'),
-  ('astroveny@gmail.com', 'Astro Veny', 'astroveny' ,'MOCK'),
-  ('buzz@gmail.com', 'Buzz Lightyear', 'buzz' ,'MOCK');
+  ('slinky@gmail.com', 'Slinky Dog', 'slinky' ,'9c9c5996-43f9-4883-ad2f-78e60d7969ec'),
+  ('buzz@gmail.com', 'Buzz Lightyear', 'buzz' ,'cd376283-0566-45bd-9403-cb3ef06c92b6');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
